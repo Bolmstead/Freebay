e852e70425a45e1b5d56d40ed67f2a6acd34f56c
Bolmstead <olms2074@gmail.com>
2026-06-16 18:55:30 -0600

    .

---
+++ build/precache-manifest.f223ca9e1ac0b2a2920693999446642d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cb47a41ec802e71bf06fa196c6c8ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c352ed90cb0fa892444",
+    "url": "/static/js/2.e71cc4a7.chunk.js"
+  },
+  {
+    "revision": "2a1aca0c3b32df831148d49a25aafd71",
+    "url": "/static/js/2.e71cc4a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b4098e90ade0f5c89c",
+    "url": "/static/js/3.7efaed87.chunk.js"
+  },
+  {
+    "revision": "d574bb29fea1a3f79f17",
+    "url": "/static/js/main.e6dfab13.chunk.js"
+  },
+  {
+    "revision": "a74ab36f366f782184e4",
+    "url": "/static/js/runtime-main.12b507b4.js"
+  }
+]);
